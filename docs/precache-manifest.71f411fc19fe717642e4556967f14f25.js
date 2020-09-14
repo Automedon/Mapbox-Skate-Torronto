041cc3a97c5d6e19b576676b553ca0e59982bd7c
Automedon <42299107+Automedon@users.noreply.github.com>
2020-09-15 05:50:48 +0700

    Mapbox-Skate-Torronto :skateboard:

---
+++ docs/precache-manifest.71f411fc19fe717642e4556967f14f25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "feb28e6bce1b90401c59d4ac21451818",
+    url: "/Mapbox-Skate-Torronto/index.html",
+  },
+  {
+    revision: "f19d7a7f21bcb57924bf",
+    url: "/Mapbox-Skate-Torronto/static/css/main.5a0cc7d1.chunk.css",
+  },
+  {
+    revision: "ba323318c04b960c6e2a",
+    url: "/Mapbox-Skate-Torronto/static/js/2.dbc09b22.chunk.js",
+  },
+  {
+    revision: "0fb0e672f1308cb5b7677cdb93fc42ab",
+    url: "/Mapbox-Skate-Torronto/static/js/2.dbc09b22.chunk.js.LICENSE.txt",
+  },
+  {
+    revision: "f19d7a7f21bcb57924bf",
+    url: "/Mapbox-Skate-Torronto/static/js/main.4e486804.chunk.js",
+  },
+  {
+    revision: "fd7d74e0bdfe325c67b7",
+    url: "/Mapbox-Skate-Torronto/static/js/runtime-main.a3365df3.js",
+  },
+  {
+    revision: "7010d550d6b9af3e26a053d6e6fb10fe",
+    url: "/Mapbox-Skate-Torronto/static/media/skate-side-view.7010d550.svg",
+  },
+]);
--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useEffect, useState } from "react";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
+import * as parkData from "./data.json";
+import skate from "./skate-side-view.svg";
+import "./App.css";
 
 function App() {
+  const [viewport, setViewPort] = useState({
+    latitude: 45.45211,
+    longitude: -75.6903,
+    width: "100vw",
+    height: "100vh",
+    zoom: 10,
+  });
+  const [selectedPark, setSelectedPark] = useState(null);
+  const handleClick = useCallback((event, park) => {
+    event.preventDefault();
+    setSelectedPark(park);
+  }, []);
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        setSelectedPark(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactMapGl
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_API_KEY}
+        mapStyle={"mapbox://styles/valefar/ckf2kcptu29ou1aljqkudpwzx"}
+        onViewportChange={(viewport) => {
+          setViewPort(viewport);
+        }}
+      >
+        {parkData.features.map((park) => (
+          <Marker
+            key={park.properties.PARK_ID}
+            latitude={park.geometry.coordinates[1]}
+            longitude={park.geometry.coordinates[0]}
+          >
+            <button
+              className={"markerButton"}
+              onClick={(e) => handleClick(e, park)}
+            >
+              <img src={skate} alt="skate park icon" />
+            </button>
+          </Marker>
+        ))}
+        {selectedPark && (
+          <Popup
+            latitude={selectedPark.geometry.coordinates[1]}
+            longitude={selectedPark.geometry.coordinates[0]}
+            onClose={() => setSelectedPark(null)}
+          >
+            <div>
+              <h2>{selectedPark.properties.NAME}</h2>
+              <h3>{selectedPark.properties.ADDRESS}</h3>
+              <p>
+                {selectedPark.properties.DESCRIPTION}
+                <br />
+                {selectedPark.properties.FACILITY}
+              </p>
+            </div>
+          </Popup>
+        )}
+      </ReactMapGl>
     </div>
   );
 }
